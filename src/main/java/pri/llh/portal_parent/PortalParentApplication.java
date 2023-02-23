34353283c9ce91180b5b0cf11f9c0205216d59c2
liaolihao <3144743728@qq.com>
2023-02-23 16:38:21 +0000

    Initial commit

---
+++ src/main/java/pri/llh/portal_parent/PortalParentApplication.java
@@ -0,0 +1,13 @@
+package pri.llh.portal_parent;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class PortalParentApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(PortalParentApplication.class, args);
+//    }
+//
+//}
---
+++ src/test/java/pri/llh/portal_parent/PortalParentApplicationTests.java
@@ -0,0 +1,13 @@
+package pri.llh.portal_parent;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PortalParentApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
